b5f4ae0804826c477c042fa03485e0d34c482880
koeyhi <kwon37954@gmail.com>
2025-02-03 10:45:04 +0900

    [Silver II] Title: 최대 힙, Time: 120 ms, Memory: 38340 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -8,10 +8,10 @@ heap = []
 for _ in range(n):
     x = int(input())
     if x > 0:
-        heapq.heappush(heap, (-x, x))
+        heapq.heappush(heap, -x)
     else:
         if len(heap) > 0:
-            print(heapq.heappop(heap)[1])
+            print(-heapq.heappop(heap))
         else:
             print(0)
             
