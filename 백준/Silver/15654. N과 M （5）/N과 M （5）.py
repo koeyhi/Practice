ff60cb215e82c8fb0865db499d63a631f2b12754
koeyhi <kwon37954@gmail.com>
2025-02-07 10:51:49 +0900

    Update N과 M （5）.py

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,7 +1,31 @@
-from itertools import permutations
+# from itertools import permutations
 
-n, m = map(int, input().split())
+# n, m = map(int, input().split())
+# nums = list(map(int, input().split()))
+
+# for i in sorted(permutations(nums, m)):
+#     print(' '.join(map(str, i)))
+
+def backtrack(depth):
+    if depth == M:
+        print(*result)
+        return
+    
+    for i in range(N):
+        if not visited[i]:  # 방문하지 않은 경우만 선택
+            visited[i] = True
+            result.append(nums[i])
+            backtrack(depth + 1)
+            result.pop()  # 원상복구 (백트래킹)
+            visited[i] = False
+
+# 입력 처리
+N, M = map(int, input().split())
 nums = list(map(int, input().split()))
+nums.sort()  # 오름차순 정렬
 
-for i in sorted(permutations(nums, m)):
-    print(' '.join(map(str, i)))
+# 백트래킹을 위한 초기 설정
+visited = [False] * N
+result = []
+
+backtrack(0)  # 백트래킹 시작
