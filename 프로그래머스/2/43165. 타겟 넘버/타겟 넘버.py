d7737b5e34c78bd1af045c8f15654d66942d036f
koeyhi <kwon37954@gmail.com>
2025-05-04 13:44:28 +0900

    [level 2] Title: 타겟 넘버, Time: 361.43 ms, Memory: 8.99 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,13 @@
+def solution(numbers, target):
+    answer = 0
+    
+    def dfs(idx, current_sum):
+        if idx == len(numbers):
+            return 1 if current_sum == target else 0
+        
+        return (
+            dfs(idx + 1, current_sum + numbers[idx]) +
+            dfs(idx + 1, current_sum - numbers[idx])
+        )
+    
+    return dfs(0, 0)
