c0e1164e15561c7b404af083fa30aa6fc0bbb9d0
koeyhi <kwon37954@gmail.com>
2025-05-05 13:47:14 +0900

    [level 2] Title: 타겟 넘버, Time: 374.14 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,11 @@
 def solution(numbers, target):
-    answer = 0
-    
     def dfs(idx, current_sum):
-        if idx == len(numbers):  # 모든 숫자를 다 사용했을 때
-            return 1 if current_sum == target else 0  # 합이 타겟과 같으면 1 아니면 0
+        if idx == len(numbers):
+            return 1 if current_sum == target else 0
         
-        # 현재 숫자를 더하는 경우와 빼는 경우 모두 탐색 및 더해서 반환
         return (
-            dfs(idx + 1, current_sum + numbers[idx]) +
+            dfs(idx + 1, current_sum + numbers[idx]) + 
             dfs(idx + 1, current_sum - numbers[idx])
         )
-    
+        
     return dfs(0, 0)
-
-# DFS 로 풀이 (재귀 호출)
