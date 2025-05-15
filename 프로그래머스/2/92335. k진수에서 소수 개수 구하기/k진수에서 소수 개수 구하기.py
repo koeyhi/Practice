8abd992583320f2957cbd4c66e98f6da79de6928
koeyhi <kwon37954@gmail.com>
2025-05-15 10:46:46 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 96.91 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,24 +1,14 @@
-import math
-
 def to_base_k(n, k):
     if n < k:
         return str(n)
     return to_base_k(n // k, k) + str(n % k)
 
 def is_prime(n):
-    if n <= 1:
+    if n < 2:
         return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    
-    l = int(math.isqrt(n))
-    i = 5
-    while i <= l:
-        if n % i == 0 or n % (i + 2) == 0:
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
             return False
-        i += 6
     return True
     
 def solution(n, k):
