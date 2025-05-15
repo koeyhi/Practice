faaeb6efb147d798c6ead73635048271613b56f6
koeyhi <kwon37954@gmail.com>
2025-05-15 16:52:04 +0900

    [level 1] Title: 유연근무제, Time: 2.69 ms, Memory: 9.6 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -1,9 +1,8 @@
 def solution(schedules, timelogs, startday):
     answer = 0
     n = len(schedules)
-    m = len(timelogs[0])
     max_time = []
-    weekday = [0 if ((d + startday) % 7 in (0, 6)) else 1 for d in range(m)]
+    weekday = [0 if ((d + startday) % 7 in (0, 6)) else 1 for d in range(7)]
     
     for i in range(n):
         if (schedules[i] + 10) % 100 >= 60:
@@ -12,8 +11,8 @@ def solution(schedules, timelogs, startday):
             max_time.append(schedules[i] + 10)
     
     for i in range(n):
-        event = [0] * m
-        for j in range(m):
+        event = [0] * 7
+        for j in range(7):
             if ((j + startday) % 7 not in (0, 6)) and timelogs[i][j] <= max_time[i]:
                 event[j] = 1
         
