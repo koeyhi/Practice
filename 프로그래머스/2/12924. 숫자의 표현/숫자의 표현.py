6ffd7c498c76ad7fc39d07732a2a196ecd40823d
koeyhi <kwon37954@gmail.com>
2025-05-23 17:56:47 +0900

    [level 2] Title: 숫자의 표현, Time: 0.05 ms, Memory: 9.08 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    k = 1
+    while k * (k - 1) // 2 < n:
+        if (n - k * (k - 1) // 2) % k == 0:
+            answer += 1
+        k += 1
+        
+    return answer
