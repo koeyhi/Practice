9a1b506fbb9b4fba7fdc90f4a79678113133ad4e
koeyhi <kwon37954@gmail.com>
2025-05-27 10:22:32 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v) # 각 컴퓨터에 연결된 컴퓨터 번호를 리스트로 저장
+    graph[v].append(u)
+
+visited = [False] * (n + 1)
+queue = deque([1])
+visited[1] = True
+count = 0
+
+while queue:
+    node = queue.popleft()
+    for neighbor in graph[node]:
+        if not visited[neighbor]:
+            queue.append(neighbor)
+            visited[neighbor] = True
+            count += 1  # 새로 감염된 컴퓨터로 카운트
+
+print(count)
