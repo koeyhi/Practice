e6dc53afb6a50fd3d34baab23d463403f41f403e
koeyhi <kwon37954@gmail.com>
2025-09-27 13:33:42 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.20 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,27 +1,36 @@
+# def solution(data, ext, val_ext, sort_by):
+#     col = 0
+#     content_condition = []
+    
+#     if ext == "code":
+#         col = 0
+#     elif ext == "date":
+#         col = 1
+#     elif ext == "maximum":
+#         col = 2
+#     else:
+#         col = 3
+    
+#     for i in range(len(data)):
+#         if data[i][col] < val_ext:
+#             content_condition.append(data[i])
+    
+#     if sort_by == "code":
+#         content_condition.sort(key=lambda x: x[0])
+#     elif sort_by == "date":
+#         content_condition.sort(key=lambda x: x[1])
+#     elif sort_by == "maximum":
+#         content_condition.sort(key=lambda x: x[2])
+#     else:
+#         content_condition.sort(key=lambda x: x[3])
+    
+#     return content_condition
+
 def solution(data, ext, val_ext, sort_by):
-    col = 0
-    content_condition = []
+    col_map = {"code": 0, "date": 1, "maximum": 2, "remain": 3}
     
-    if ext == "code":
-        col = 0
-    elif ext == "date":
-        col = 1
-    elif ext == "maximum":
-        col = 2
-    else:
-        col = 3
+    filtered_data = [d for d in data if d[col_map[ext]] < val_ext]
     
-    for i in range(len(data)):
-        if data[i][col] < val_ext:
-            content_condition.append(data[i])
+    answer = sorted(filtered_data, key=lambda x: x[col_map[sort_by]])
     
-    if sort_by == "code":
-        content_condition.sort(key=lambda x: x[0])
-    elif sort_by == "date":
-        content_condition.sort(key=lambda x: x[1])
-    elif sort_by == "maximum":
-        content_condition.sort(key=lambda x: x[2])
-    else:
-        content_condition.sort(key=lambda x: x[3])
-    
-    return content_condition
+    return answer
